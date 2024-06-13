dac19bd2d2cdb67fb56a9a669e469ebf220e4709
Christian Solomon <86303900+chris-zano@users.noreply.github.com>
2024-06-13 23:54:21 +0000

    added segregation of email addresses [ valid would be processed and mailed to ]

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,9 +1,11 @@
 const { Customers, Files } = require('../utils/db.exports.utils');
 const mailer = require('../utils/mailer.utils');
+const emailregexp = mailer.emailRegexp();
 
 module.exports.shareFileController = async (req, res) => {
     const { id, subject, message, receipients } = req.body;
+    const validReceipientEmails = Array.isArray(receipients) ? receipients.filter((receipient) => (emailregexp.test(receipient))) : [];
 
-    console.log({ id, subject, message, receipients });
+    console.log({ id, subject, message, validReceipientEmails });
     res.status(200).json({message: "received"});
 }
